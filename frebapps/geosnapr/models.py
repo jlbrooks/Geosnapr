352806f0d2663fb7431ec020ce181cfaad76f933
Jacob Brooks <jacob@chrisbrooks.org>
2015-11-01 19:45:18 -0500

    Added geosnapr to apps, migrate image field

--- frebapps/geosnapr/models.py
+++ frebapps/geosnapr/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to="pics")
+
+    lat = models.DecimalField(max_digits=8, decimal_places=6)
+
+    lng = models.DecimalField(max_digits=8, decimal_places=6)
+
+    caption = models.CharField(max_length=100)

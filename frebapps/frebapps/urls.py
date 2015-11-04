2d69d90017d2bbb863f883d696276a1f70119430
guochenshen <guochenshen@gmail.com>
2015-11-04 17:55:49 -0500

    there's a map!

--- frebapps/frebapps/urls.py
+++ frebapps/frebapps/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     url(r'^login$', views.login_view, name='login'),
     url(r'^logout$', views.logout_view, name='logout'),
     url(r'^register$', views.register, name='register'),
+    url(r'^map$', views.main_map, name='map'),
     url(r'^$', views.index, name='index'),
 ]

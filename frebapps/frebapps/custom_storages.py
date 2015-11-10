ad673e54a828d326ecef317c69441345bf157320
Jacob Brooks <jacob@chrisbrooks.org>
2015-11-10 15:53:01 -0500

    Fix indent

--- frebapps/frebapps/custom_storages.py
+++ frebapps/frebapps/custom_storages.py
@@ -4,5 +4,5 @@ from storages.backends.s3boto import S3BotoStorage
 class StaticStorage(S3BotoStorage):
     location = settings.STATICFILES_LOCATION
 
- class MediaStorage(S3BotoStorage):
+class MediaStorage(S3BotoStorage):
     location = settings.MEDIAFILES_LOCATION

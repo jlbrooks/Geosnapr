1bed976b18be37fd5947355e26c06ef3c6b68258
Jacob Brooks <jacob@chrisbrooks.org>
2015-11-19 18:10:29 -0500

    Added pip line to build script

--- frebapps/scripts/start_application.sh
+++ frebapps/scripts/start_application.sh
@@ -3,6 +3,9 @@
 # Change to the frebapps directory
 cd /opt/Team177/frebapps/
 
+# Use pip to install dependencies
+pip install -r requirements.txt
+
 # Copy settings
 cp frebapps/settings.prod.py frebapps/settings.py
 

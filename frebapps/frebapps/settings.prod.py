94c0c820fc756dbd6ad8591849a66f18714d7c80
Jacob Brooks <jacob@chrisbrooks.org>
2015-11-22 16:17:44 -0500

    No email logging

--- frebapps/frebapps/settings.prod.py
+++ frebapps/frebapps/settings.prod.py
@@ -106,14 +106,6 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
-        # Include the default Django email handler for errors
-        # This is what you'd get without configuring logging at all.
-        'mail_admins': {
-            'class': 'django.utils.log.AdminEmailHandler',
-            'level': 'ERROR',
-             # But the emails are plain text by default - HTML is nicer
-            'include_html': True,
-        },
         # Log to a text file that can be rotated by logrotate
         'logfile': {
             'class': 'logging.handlers.WatchedFileHandler',
@@ -121,12 +113,6 @@ LOGGING = {
         },
     },
     'loggers': {
-        # Again, default Django configuration to email unhandled exceptions
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
         # Might as well log any errors anywhere else in Django
         'django': {
             'handlers': ['logfile'],

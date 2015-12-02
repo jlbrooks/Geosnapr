8d8c4fd08b7b3887b1e282aa258bba33c2e8387d
guochenshen <guochenshen@gmail.com>
2015-12-02 06:41:07 -0500

    split up js files

---
+++ frebapps/geosnapr/static/js/maps-display.js
@@ -0,0 +1,341 @@
+///////////////////////////////
+// Alert functions
+///////////////////////////////
+
+function fadeAlert() {
+  var msg = $("#message");
+  msg.fadeOut(500, function () {
+    msg.removeClass();
+    msg.addClass("alert-box");
+    msg.html();
+  });
+}
+
+function alertSuccess(content) {
+  var msg = $("#message");
+  msg.addClass("success");
+  msg.html(content);
+  msg.fadeIn(500);
+
+  // Fade the alert
+  setTimeout(fadeAlert, 3000);
+}
+
+function geocodeForm(lat, lng, latElem, lngElem, resultElem) {
+  $(latElem).val(lat);
+  $(lngElem).val(lng);
+
+  // Try to reverse geocode
+  var geocoder = new google.maps.Geocoder;
+  var loc = {
+    lat: lat,
+    lng: lng
+  };
+
+  geocoder.geocode({'location': loc}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      if (results[1]) {
+        $(resultElem).val(results[1].formatted_address);
+      }
+    } else {
+      console.log("Geocoder failed due to: " + status);
+    }
+  });
+}
+
+function imageChosen(input) {
+  var reader = new FileReader();
+
+  reader.onload = function (e) {
+    // get loaded data and render thumbnail.
+    document.getElementById("upload-img").src = e.target.result;
+  };
+
+  // read the image file as a data URL.
+  reader.readAsDataURL(input.files[0]);
+
+  $(input).fileExif(function (exifObject) {
+    console.log(exifObject);
+
+    if (exifObject.GPSLatitude && exifObject.GPSLongitude) {
+      var latArray = exifObject.GPSLatitude;
+      var lngArray = exifObject.GPSLongitude;
+
+      // Convert lat/lng to decimal
+      var latDecimal = latArray[0] + (latArray[1]/60) + (latArray[2]/3600);
+
+      var lngDecimal = lngArray[0] + (lngArray[1]/60) + (lngArray[2]/3600);
+
+      // N/S/E/W
+      if (exifObject.GPSLatitudeRef === "S") {
+        latDecimal = latDecimal * -1;
+      }
+      if (exifObject.GPSLongitudeRef === "W") {
+        lngDecimal = lngDecimal * -1;
+      }
+
+      geocodeForm(latDecimal, lngDecimal, $("#autolat"), $("#autolng"), $('#imagelocation'));
+    }
+  });
+
+  $("#img-loc-form").removeClass("hidden");
+}
+
+function edit_profile(event) {
+  event.preventDefault();
+  var form = $("#edit-profile-form");
+
+  $.ajax({
+    type: form.attr('method'),
+    url: form.attr('action'),
+    data: form.serialize(),
+    success: function (data) {
+      var msg = $('#profile-notification');
+
+      if (data.errors.length > 0) {
+        msg.html(data.errors[0]);
+        msg.addClass("error");
+      } else {
+        alertSuccess(data.msg);
+        // Close the modal
+        $('#editProfileModal').foundation('reveal', 'close');
+      }
+      msg.removeClass("hidden");
+    },
+    error: function (data) {
+      console.log(data);
+    }
+  });
+}
+
+function clearImageForm() {
+  $("#autolat").val('');
+  $("#autolng").val('');
+  $("#imagelocation").val('');
+  $("#caption").val('');
+}
+
+function upload_image(event) {
+  event.preventDefault();
+  var form = $("#upload-img-form");
+  var formData = new FormData(document.getElementById("upload-img-form"));
+  formData.append("upload-album", $("#upload-album").val());
+  // Are we in the instagram tab?
+  if ($("#panel2").attr('aria-hidden') == 'false') {
+    formData.append('external', true);
+    formData.append('url', $("#selected-img").attr('src'));
+  }
+  // Set up the loading spinner
+  var opts = {
+    scale: 2.5,
+    top: '50%',
+    left: '50%'
+  };
+  var spinner = new Spinner(opts).spin();
+
+  // Show the loading spinner
+  form.append(spinner.el);
+
+ $.ajax({
+    type: form.attr('method'),
+    url: form.attr('action'),
+    data: formData,
+    processData: false,
+    contentType: false,
+    success: function (data) {
+      // Add a new marker
+      addMarkers([data.image]);
+      // Remove the form dataå
+      clearImageForm();
+      //$("#upload-img").val(null);
+      $("#upload-img").attr('src', '');
+      $("#upload-file").replaceWith($("#upload-file").clone(true));
+      // Close the modal
+      $('#uploadModal').foundation('reveal', 'close');
+      // Stop the spinner
+      spinner.stop();
+      alertSuccess(data.message);
+    },
+    error: function (data) {
+      console.log(data);
+      console.log("fail");
+    }
+  });
+}
+
+function delete_image() {
+  event.preventDefault();
+  var id = $("#img-id").val();
+  var data = {
+    "img_id": id
+  }
+
+  $.ajax({
+    type: "POST",
+    url: $(this).attr('href'),
+    data: data,
+    success: function (data) {
+      // For now, redirect to this page
+      location.reload();
+    },
+    error: function (data) {
+      console.log(data);
+    }
+  })
+}
+
+function edit_image() {
+  event.preventDefault();
+  var form = $("#img-edit-form");
+  var formData = new FormData(document.getElementById("img-edit-form"));
+
+  // Set up the loading spinner
+  var opts = {
+    scale: 2.5,
+    top: '50%',
+    left: '50%'
+  };
+  var spinner = new Spinner(opts).spin();
+
+  // Show the loading spinner
+  form.append(spinner.el);
+
+  $.ajax({
+    type: form.attr('method'),
+    url: form.attr('action'),
+    data: formData,
+    processData: false,
+    contentType: false,
+    success: function(data) {
+      for (var i = 0; i < allmarkers.length; i++) {
+        var marker = allmarkers[i];
+        if (marker.image.id == data.image.id) {
+
+          marker.image.caption = data.image.caption;
+          marker.image.lat = data.image.lat;
+          marker.image.lng = data.image.lng;
+          marker.image.albums = data.image.albums;
+          var latlng = new google.maps.LatLng({lat:data.image.lat, lng:data.image.lng});
+          marker.setPosition(latlng);
+        }
+      }
+
+      show_album();
+      // Close the modal
+      $('#uploadModal').foundation('reveal', 'close');
+      // Stop the spinner
+      spinner.stop();
+      alertSuccess(data.message);
+    },
+    error: function(data) {
+      console.log('error');
+      spinner.stop();
+      console.log(data);
+    }
+  })
+}
+
+function create_album() {
+  event.preventDefault();
+  var form = $("#create-album-form");
+  var formData = new FormData(document.getElementById("create-album-form"));
+
+  // Set up the loading spinner
+  var opts = {
+    scale: 2.5,
+    top: '50%',
+    left: '50%'
+  };
+  var spinner = new Spinner(opts).spin();
+
+  // Show the loading spinner
+  form.append(spinner.el);
+
+  $.ajax({
+    type: form.attr('method'),
+    url: form.attr('action'),
+    data: formData,
+    processData: false,
+    contentType: false,
+    success: function(data) {
+      // Append the new album to the dropdown
+      var option = new Option(data.album.name, data.album.id);
+      $(option).html(data.album.name);
+      $("#map-albums").append(option);
+      $("#upload-album").append($(option).clone());
+      $("#edit-album").append($(option).clone());
+      // Clear the form data
+      $("#album-name").val('Untitled Album')
+      // Close the modal
+      $('#uploadModal').foundation('reveal', 'close');
+      // Stop the spinner
+      spinner.stop();
+      alertSuccess(data.message);
+    },
+    error: function(data) {
+
+    }
+  });
+}
+
+function openImageEditForm(image) {
+  $("#img-edit-form-hidden").hide();
+  $("#img-edit-form-show").show();
+  $("#img-edit-form-show").on("click", function() {
+    $("#img-edit-form-hidden").show();
+    $("#img-edit-form-show").hide();
+  });
+  $('#photo-modal-link').attr("src",image.image.replace(/&amp;/g, '&'));
+  $('#photo-modal-comment').text(image.caption);
+  $("#editcaption").val(image.caption);
+  $("#img-id").val(image.id);
+  $("#edit-album").val(image.albums);
+  geocodeForm(image.lat, image.lng, $("#autoeditlat"), $("#autoeditlng"), $('#imageeditlocation'));
+
+  $('#photo-modal').foundation('reveal','open');
+  $("#edit-img-btn").unbind('click');
+  $("#edit-img-btn").on('click', edit_image);
+}
+
+function show_album() {
+  var id = $('#map-albums').val();
+  markerclusterer.clearMarkers();
+
+  $.ajax({
+    type: "POST",
+    url: "get_album",
+    data: {'a_id':id},
+    success: function(data) {
+      var images = data.images;
+      for (var i = 0; i < allmarkers.length; i++) {
+        var marker = allmarkers[i];
+        var check = parseInt(marker.photoid);
+        if (images.indexOf(check) > -1) {
+          markerclusterer.addMarker(marker);
+        }
+      }
+
+      var markers = markerclusterer.getMarkers();
+
+      if (markers.length > 0) {
+        console.log('resizing');
+        var bounds = new google.maps.LatLngBounds();
+        for (var i = 0; i < markers.length; ++i) {
+          var marker = markers[i];
+          bounds.extend(marker.position);
+        }
+        map.fitBounds(bounds);
+        var listener = google.maps.event.addListenerOnce(map, "idle", function() {
+            if (map.getZoom() > 8) map.setZoom(8);
+        });
+      }
+      else {
+        map.setCenter(new google.maps.LatLng(40, -79));
+        map.setZoom(8);
+      }
+    },
+    error: function(data) {
+      console.log(data);
+    }
+  });
+}
---
+++ frebapps/geosnapr/static/js/maps-markers.js
@@ -0,0 +1,101 @@
+function hideImageInfoWindow(marker) {
+  // should always be defined if function is called
+  if (marker.infoWindow != undefined) {
+    marker.infoWindow.close();
+  }
+}
+
+function showImageInfoWindow(map, marker) {
+  var htmlcontent = `
+  <div class="thumbnail-container">
+    <img border="0" class="thumbnail" src="` + marker.image.image + `">
+  </div>`;
+
+  var infobubble = new InfoBubble({
+    maxWidth: 300,
+    minWidth: 300,
+    content: htmlcontent,
+    disableAutoPan: true,
+    hideCloseButton: true,
+    borderWidth: 0,
+    padding: 0,
+    pixelOffset: [0,0],
+  })
+
+  infobubble.open(map, marker);
+  if (marker.infoWindow != undefined) {
+    marker.infoWindow.close();
+  }
+  marker.infoWindow = infobubble;
+}
+
+function addMarkers(json) {
+  // adds each image object in the json object to the markerclusterer
+  for (var i = 0; i < json.length; i++) {
+    var image = json[i];
+    var latitude = image['lat'];
+    var longitude = image['lng'];
+    var id = image['id'];
+    var albums = image['albums'];
+
+    var latlng = new google.maps.LatLng({lat:latitude, lng:longitude});
+    // creates new marker
+    var marker = new google.maps.Marker({
+      position:latlng,
+      icon: 'static/img/marker_picture_small.png'
+    });
+
+    marker.image = image;
+    marker.photoid = id;
+
+    allmarkers.push(marker);
+    allmarkerspublic.push(marker);
+    console.log(allmarkerspublic);
+  }
+
+  for (var i = 0; i < json.length; ++i) {
+    var markers = allmarkers;
+    var key = imagecount;
+
+    google.maps.event.addListener(markers[key],'mouseover', function(key2) {
+      return function() {
+        showImageInfoWindow(map, markers[key2]);
+      }
+    }(key));
+
+    google.maps.event.addListener(markers[key],'mouseout', function(key2) {
+      return function() {
+        hideImageInfoWindow(markers[key2]);
+      }
+    }(key));
+
+    google.maps.event.addListener(markers[key],'click', function(key2) {
+      return function() {
+        var image = markers[key2].image;
+        openImageEditForm(image);
+      }
+    }(key));
+
+
+    imagecount++;
+  }
+  show_album();
+  var markers2 = markerclusterer.getMarkers();
+
+  // rezooms based on what was added
+  if (markers2.length > 0) {
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0; i < markers.length; ++i) {
+      var marker = markers2[i];
+      bounds.extend(marker.position);
+    }
+    map.fitBounds(bounds);
+    var listener = google.maps.event.addListenerOnce(map, "idle", function() {
+        if (map.getZoom() > 8) map.setZoom(8);
+    });
+  }
+  else {
+    map.setCenter(new google.maps.LatLng(40, -79));
+    map.setZoom(8);
+  }
+}

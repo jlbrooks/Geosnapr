80d88d984fb2b6d837e6f0ac54269bba5d75a8cd
guochenshen <guochenshen@gmail.com>
2015-12-03 04:12:13 -0500

    barebones 404 and 500 pages

--- frebapps/geosnapr/views/main.py
+++ frebapps/geosnapr/views/main.py
@@ -125,3 +125,13 @@ def delete_profile(request):
     request.user.delete()
     # Redirect to front page
     return redirect(index)
+
+def handler404(request):
+    response = render_to_response('404.html', {})
+    response.status_code = 404
+    return response
+
+def handler500(request):
+    response = render_to_response('500.html', {})
+    response.status_code = 500
+    return response
